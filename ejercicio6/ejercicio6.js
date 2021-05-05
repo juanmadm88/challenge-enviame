a3b7bfa25d2c1b14d88f83d1b9db7da2ce47973e
martin <jdimartino@simtlix.com>
2021-05-05 12:55:36 -0300

    se saco la logica cableada de switch para calcular la serie que devuelve la cantidad de dias

--- ejercicio6/ejercicio6.js
+++ ejercicio6/ejercicio6.js
@@ -5,81 +5,17 @@ let generateKilometers = () =>{
     return Math.round(Math.random() * MAX_KILOMETERS);
 }
 
-let calculateSuccession = (aNumber) => {
-    return (aNumber < 2) ? aNumber : calculateSuccession(aNumber-2 ) + calculateSuccession(aNumber-1);
+let calculateDeliveryDay = (aNumber) => {
+    return (aNumber < 2) ? aNumber : calculateDeliveryDay(aNumber-2 ) + calculateDeliveryDay(aNumber-1);
 }
 
-let calculateDeliveryDay = (someKilometers)=>{
-    let deliveryDays;
-    switch(true) {
-        case someKilometers < 100:
-            deliveryDays = calculateSuccession(0);
-            break;
-        case someKilometers < 200:
-            deliveryDays = calculateSuccession(1);
-            break;
-        case someKilometers < 300:
-            deliveryDays = calculateSuccession(2);
-            break;
-        case someKilometers < 400:
-            deliveryDays = calculateSuccession(3);
-            break;
-        case someKilometers < 500:
-            deliveryDays = calculateSuccession(4);
-            break;
-        case someKilometers < 600:
-            deliveryDays = calculateSuccession(5);
-            break;
-        case someKilometers < 700:
-            deliveryDays = calculateSuccession(6);
-            break;
-        case someKilometers < 800:
-            deliveryDays = calculateSuccession(7);
-            break;
-        case someKilometers < 900:
-            deliveryDays = calculateSuccession(8);
-            break;
-        case someKilometers < 1000:
-            deliveryDays = calculateSuccession(9);
-            break;
-        case someKilometers < 1100:
-            deliveryDays = calculateSuccession(10);
-            break;
-        case someKilometers < 1200:
-            deliveryDays = calculateSuccession(11);
-            break;
-        case someKilometers < 1300:
-            deliveryDays = calculateSuccession(12);
-            break;
-        case someKilometers < 1400:
-            deliveryDays = calculateSuccession(13);
-            break;
-        case someKilometers < 1500:
-            deliveryDays = calculateSuccession(14);
-            break;
-        case someKilometers < 1600:
-            deliveryDays = calculateSuccession(15);
-            break;
-        case someKilometers < 1700:
-            deliveryDays = calculateSuccession(16);
-            break;
-        case someKilometers < 1800:
-            deliveryDays = calculateSuccession(17);
-            break;
-        case someKilometers < 1900:
-            deliveryDays = calculateSuccession(18);
-            break;
-        case someKilometers < 2000:
-            deliveryDays = calculateSuccession(19);
-            break;
-        default:
-            deliveryDays = calculateSuccession(0);
-            break;
-    }
-    return deliveryDays;
+let calculateOrder = (kilometers) => {
+    return Math.round(kilometers/100);
 }
 
 let kilometers = generateKilometers();
-let deliveryDays = calculateDeliveryDay(kilometers);
+let order = calculateOrder(kilometers);
+console.log("order ", order);
+let deliveryDays = calculateDeliveryDay(order);
 
 console.log(`El tiempo de entrega, será de apróximadamente  ${deliveryDays} días`)
